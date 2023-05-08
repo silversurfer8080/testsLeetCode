9b8c0aacc8e88e13481c914df8edee94863d93ce
silversurfer8080 <gilberto01@hotmail.com>
2023-05-08 16:28:02 -0300

    Leetcode problem 242

--- src/arraysandhashing/ValidAnagram242.java
+++ src/arraysandhashing/ValidAnagram242.java
@@ -1,13 +1,14 @@
 package src.arraysandhashing;
 
+import java.util.Arrays;
+
 /**
  * Leetcode problem number 242.
  * Valid Anagram.
  */
 public class ValidAnagram242 {
     public static boolean isAnagram(String one, String two) {
-        if (one == null || two == null) return false;
-        if (one.length() != two.length()) return false;
+        if (checkNullandSize(one, two)) return false;
         int end = one.length() - 1;
         for (int start = 0; start < one.length(); start++) {
             if (one.charAt(start) != two.charAt(end)) {
@@ -17,4 +18,42 @@ public class ValidAnagram242 {
         }
         return false;
     }
+
+    public static boolean isAnagramUsingSorting(String one, String two) {
+        if (checkNullandSize(one, two)) return false;
+
+        char[] charOne = one.toCharArray();
+        char [] charTwo = two.toCharArray();
+
+        Arrays.sort(charOne);
+        Arrays.sort(charTwo);
+
+        return Arrays.equals(charOne,charTwo);
+    }
+
+    private static boolean checkNullandSize(String one, String two) {
+        if (one == null || two == null) return true;
+        if (one.length() != two.length()) return true;
+        return false;
+    }
+
+    public static boolean isAnagramCounting(String one, String two){
+        int CHARACTER_RANGE= 256;
+        if (checkNullandSize(one, two)) return false;
+
+        int count[] = new int[CHARACTER_RANGE];
+        for (int i = 0; i < one.length(); i++) {
+            count[one.charAt(i)]++;
+
+            count[two.charAt(i)]--;
+        }
+        for (int i = 0; i < CHARACTER_RANGE; i++) {
+                if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
--- src/main/java/src/main/java/Main.java
+++ src/main/java/src/main/java/Main.java
@@ -1,10 +1,12 @@
 package src.main.java;
 import src.arraysandhashing.ContainsDuplicateProblem;
+import src.arraysandhashing.ValidAnagram242;
 
 public class Main {
 
     public static void main (String[] args){
-        int[] nums = new int[]{1,2,3,4,4,5,5,2};
-        System.out.println(ContainsDuplicateProblem.solveWithStream(nums));
+//        int[] nums = new int[]{1,2,3,4,4,5,5,2};
+//        System.out.println(ContainsDuplicateProblem.solveWithStream(nums));
+        ValidAnagram242.isAnagramCounting("abc", "cba");
     }
 }
