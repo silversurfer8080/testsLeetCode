85092466725e68cc9c0578b0719c9529a260120c
silversurfer8080 <gilberto01@hotmail.com>
2023-05-11 16:10:18 -0300

    Leetcode problem 347

---
+++ src/arraysandhashing/TopKFrequentElements347.java
@@ -0,0 +1,26 @@
+package src.arraysandhashing;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequentElements347 {
+    public static int[] findTopKFrequentElements(int[] nums, int k) {
+        int[] arr = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+                (a, b) ->
+                        a.getValue() - b.getValue()
+        );
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+            pq.add(it);
+            if (pq.size() > k) pq.poll();
+        }
+        int i = k;
+        while (!pq.isEmpty()) {
+            arr[--i] = pq.poll().getKey();
+        }
+        return arr;
+    }
+}
